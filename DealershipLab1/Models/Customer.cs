306408ca147c62ece8ee3300552558b4c0c8353a
cherydactyl <cheryl.orosz@gmail.com>
2015-07-27 11:47:36 -0400

    Added Customer Class
    references AutoModel class

---
+++ DealershipLab1/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DealershipLab1.Models
+{
+    //class to store customer information and be the basis of our customer database
+    public class Customer
+    {
+        public string firstName { get; set; }   //First name of customer
+        public string lastName { get; set; }    //last name of customer
+
+        public AutoModel carPurchased;          //link to car that is purchased, if any.  
+    }
+}
